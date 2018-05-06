a8af15b6b14c118ba79a7602c72124f88316a4f7
Paul Bomba` <paul@paulbomba.com>
2018-05-06 18:55:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToBeginningOfArray(array, element){
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = array.unshift(element);
+  array.unshift(element);
   return array;
 }
 /*

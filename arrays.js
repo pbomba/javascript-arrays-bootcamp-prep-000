d0c7260bcea5e61cab344841b28e4a418958dc20
Paul Bomba` <paul@paulbomba.com>
2018-05-06 19:14:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,7 +44,7 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.slice(0, array.length -1);
+  array.pop();
   return array
 }
 /*

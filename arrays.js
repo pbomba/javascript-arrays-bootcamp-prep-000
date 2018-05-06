ad1778060e92b2e49da070c56a6ab557f4c24007
Paul Bomba` <paul@paulbomba.com>
2018-05-06 18:02:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ['snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+/*
+addElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return Array;
+} 
+
+destructivelyAddElementToBeginningOfArray(Array, element){
+  newArray = ...Array;
+  return newArray;
+}
+*/
